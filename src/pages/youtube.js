a7884b7d3c56f2a13b2809dcce58f26ec1b4ca9e
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-04-05 13:05:37 -0500

    API requests

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -1,1 +1,44 @@
+const API_VIDEOS =
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCilt3NJtU0tHvoFjo-OHL3A&part=snippet%2Cid&order=date&maxResults=9";
 
+const OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": API,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+async function fetchData(urlApi, obj) {
+  const response = await fetch(urlApi, obj);
+  const data = await response.json();
+  return data;
+}
+
+const containerVideos = document.querySelector(".youtube__videos");
+
+const generateVideos = async (urlApi, obj) => {
+  try {
+    const videos = await fetchData(urlApi, obj);
+    console.log(videos);
+
+    const toRender = videos.items
+      .map((element) => {
+        return `
+                <div class="youtube__videos-card">
+            <img src="${element.snippet.thumbnails.medium.url}" alt="${element.snippet.thumbnails.title}" />
+            <p> <a href="https://www.youtube.com/watch?v=${element.id.videoId}" target="_blank">${element.snippet.title}</a></p>
+          </div>
+      `;
+      })
+      .join("");
+
+    containerVideos.innerHTML = toRender;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generateVideos(API_VIDEOS, OPTIONS);
+
+//export default { generateVideos };
---
+++ src/secrets.js
@@ -0,0 +1,1 @@
+const API = "7f50e19026msh8c452a57b698e2bp1341a8jsn5ec636f2af24";
